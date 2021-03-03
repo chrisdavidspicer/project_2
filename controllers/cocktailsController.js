0ebac9f8468b690bef1540313a5479b0c8ce2ac0
CHRISDAVIDSPICER <CHRISDAVIDSPICER@GMAIL.COM>
2021-03-02 17:02:06 -0800

    Add more routes and work with API

--- controllers/cocktailsController.js
+++ controllers/cocktailsController.js
@@ -1,7 +1,31 @@
 const router = require('express').Router()
 const db = require('../models')
+const axios = require('axios')
 
-// Show all cocktails
+// Show all cocktail categories
+router.get('/', async (req, res) => {
+  try {
+    const cocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    const response = await axios.get(cocktailURL)
+    const cocktails = response.data.drinks
+    res.render('cocktails/index', { cocktails: cocktails })
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+// Show list of cocktails sorted by category
+router.get('/', async (req, res) => {
+  try {
+    const category = req.body.category
+    const cocktailURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
+    const response = await axios.get(cocktailURL)
+    const cocktails = response.data.drinks
+    res.render('cocktails/index', { cocktails: cocktails })
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 // Show info for one specific cocktail
 
